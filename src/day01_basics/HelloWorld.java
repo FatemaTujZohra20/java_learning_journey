bfceb02d3e2f24669fcb49b4141d91501717c1b0
Fatema Tuj Zohra <fatematujzohra20@gmail.com>
2026-01-03 05:52:54 +0600

    Add HelloWorld program and Java entry point

---
+++ src/day01_basics/HelloWorld.java
@@ -0,0 +1,8 @@
+package day01_basics;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("My Java learning journey begins.");
+    }
+}
