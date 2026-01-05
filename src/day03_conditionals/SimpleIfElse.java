305fd13497222fdc573edddde5637f3085e45217
Fatema Tuj Zohra <fatematujzohra20@gmail.com>
2026-01-06 02:41:29 +0600

    Day 03: add basic if-else decision example

---
+++ src/day03_conditionals/SimpleIfElse.java
@@ -0,0 +1,20 @@
+package day03_conditionals;
+
+ /*
+    * Basic decision-making
+    * Boolean condition evaluation
+    * Clean readable structure
+ */
+
+public class SimpleIfElse {
+    public static void main(String[] args) {
+
+        int age = 24;
+
+        if (age >= 18) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+    }
+}
