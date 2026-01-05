2c0ca72313f36d1558f954e05971b8438e9026a3
Fatema Tuj Zohra <fatematujzohra20@gmail.com>
2026-01-06 03:15:41 +0600

    Day 03: add input validation using conditionals

---
+++ src/day03_conditionals/InputValidation.java
@@ -0,0 +1,22 @@
+package day03_conditionals;
+
+/*
+    Learning:
+    * Defensive programming
+    * Validating inputs before using them
+    * Real-world habit (used in APIs & backend)
+ */
+public class InputValidation {
+    public static void main(String[] args) {
+
+        int age = -5;
+
+        if (age < 0) {
+            System.out.println("Invalid age! Age cannot be negative.");
+        } else if (age > 120) {
+            System.out.println("Invalid age! Age seems unrealistic.");
+        } else {
+            System.out.println("Valid age entered: " + age);
+        }
+    }
+}
