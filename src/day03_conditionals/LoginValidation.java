e82467f451f1f3a439d2f9e7c2cbe07987add0af
Fatema Tuj Zohra <fatematujzohra20@gmail.com>
2026-01-06 03:24:53 +0600

    Day 03: demonstrate logical operators in login validation

---
+++ src/day03_conditionals/LoginValidation.java
@@ -0,0 +1,22 @@
+package day03_conditionals;
+
+/*
+    Learning
+    * && (AND)
+    * || (OR)
+    * .equals() instead of == for strings
+    * Real authentication logic basics
+ */
+public class LoginValidation {
+    public static void main(String[] args) {
+
+        String username = "admin";
+        String password = "3478";
+
+        if (username.equals("admin") && password.equals("3478")) {
+            System.out.println("Login successful!");
+        } else {
+            System.out.println("Invalid username or password!");
+        }
+    }
+}
