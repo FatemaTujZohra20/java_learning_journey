d60762085cebd2f3a2b6c0404d098a35f66f13bb
Fatema Tuj Zohra <fatematujzohra20@gmail.com>
2026-01-08 20:26:59 +0600

    Day 03: introduce guard clause for clean conditional flow

---
+++ src/day03_conditionals/GuardClauseExample.java
@@ -0,0 +1,21 @@
+package day03_conditionals;
+
+/*
+    Learning:
+    * Fail fast principle
+    * Cleaner logic
+    * Used heavily in production systems
+ */
+public class GuardClauseExample {
+    public static void main(String[] args) {
+        int amount = -100;
+
+        // Guard clause: fail fast
+        if (amount <= 0) {
+            System.out.println("Invalid amount!");
+            return;
+        }
+
+        System.out.println("Processing payment of: " + amount);
+    }
+}
