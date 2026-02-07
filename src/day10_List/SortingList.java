71a4554b576572c18007b72c670717b25928857a
Fatema Tuj Zohra <fatematujzohra20@gmail.com>
2026-02-07 10:58:26 +0600

    Add Comparator usage in list sorting

---
+++ src/day10_List/SortingList.java
@@ -0,0 +1,34 @@
+package day10_List;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortingList {
+    public static void main(String[] args) {
+        // Generics with List
+        List<Integer> values = new ArrayList<>();
+        values.add(404);
+        values.add(908);
+        values.add(639);
+        values.add(265);
+
+        Comparator<Integer> c = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer i, Integer j) {
+                if (i%10>j%10)
+                    return 1;
+                else
+                    return -1;
+            }
+        };
+
+        Collections.sort(values, c);
+
+        for (Integer o : values) {
+            System.out.println(o);
+        }
+    }
+
+}
