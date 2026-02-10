a6c8416e9480da709f19728071c091d324fcdae5
Fatema Tuj Zohra <fatematujzohra20@gmail.com>
2026-02-10 20:10:35 +0600

    Add example of using Stream in code

---
+++ src/day11_Stream_API/WithStream.java
@@ -0,0 +1,22 @@
+package day11_Stream_API;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WithStream {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(10, 15, 20, 25, 30);
+
+        // Stream pipeline:
+        // 1. stream() -> convert collection to stream
+        // 2. filter() -> intermediate operation
+        // 3. collect() -> terminal operation
+        List<Integer> evenNumbers = numbers.stream()
+                                            .filter(num -> num % 2 == 0)
+                                            .collect(Collectors.toList());
+
+        System.out.println(evenNumbers);
+
+    }
+}
