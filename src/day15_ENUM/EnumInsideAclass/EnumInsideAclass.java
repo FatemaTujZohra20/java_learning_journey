02e58e9922f2ef83de82d3e225e0908630780350
Fatema Tuj Zohra <fatematujzohra20@gmail.com>
2026-02-16 19:45:31 +0600

    Add example on use case of Enum inside a class

---
+++ src/day15_ENUM/EnumInsideAclass/EnumInsideAclass.java
@@ -0,0 +1,15 @@
+package day15_ENUM.EnumInsideAclass;
+
+public class EnumInsideAclass {
+    enum Level {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+    
+    
+    public static void main (String[] args) {
+        Level myVar = Level.MEDIUM;
+        System.out.println(myVar);
+    }
+}
